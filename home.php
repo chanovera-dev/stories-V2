1e188e9e780870f163409545d54ec5956780c83a
Chano Vera <github@chanovera.com>
2025-10-29 19:08:52 -0600

    add the loop page's

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Homepage / Blog Index Template
+ *
+ * This template is used to display the homepage or blog index page.
+ * Depending on WordPress settings, it can show:
+ * - Latest posts (blog posts) on the homepage.
+ * - Featured content or sections if customized.
+ * - Pagination for navigating multiple pages of posts.
+ *
+ * It is typically the first template users see when visiting the site.
+ *
+ * @package stories
+ * @since 2.0.0
+ */
+
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
+    
+
+    <!-- Archive Posts Section -->
+    <section class="block posts--body">
+        <div class="content">
+            <?php
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post();
+                    get_template_part( 'template-parts/content', 'archive' );
+                }
+
+                the_posts_pagination( array(
+                    'mid_size'  => 2,
+                    'prev_text' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/></svg>',
+                    'next_text' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/></svg>'
+                ) );
+            } else {
+                /* translators: Message displayed when no posts are found in an archive. */
+                echo '<p>' . esc_html__( 'No se han encontrado artículos', 'stories' ) . '</p>';
+            }
+            ?>
+        </div>
+    </section>
+
+</main><!-- .site-main -->
+
+<?php get_footer(); ?>
