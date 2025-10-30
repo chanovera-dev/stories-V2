3d36964e5aafc45060dac5504209de1016ed2997
Chano Vera <github@chanovera.com>
2025-10-29 18:55:38 -0600

    add the page template

---
+++ page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Single Page Template
+ *
+ * This template is used to display individual WordPress pages.
+ * It loads the header, runs the main loop to display page content
+ * using the 'template-parts/content-page.php' template part,
+ * and then loads the footer.
+ *
+ * This template is typically used for static pages like "About Us",
+ * "Contact", or any page created in the WordPress admin.
+ *
+ * @package stories
+ * @since 2.0.0
+ */
+
+get_header();
+
+if ( have_posts() ) {
+
+    while( have_posts() ) {
+
+        the_post();
+        get_template_part( 'template-parts/content', 'page' );
+        
+    }
+
+}
+
+get_footer();
---
+++ template-parts/content-page.php
@@ -0,0 +1,24 @@
+<div id="main" class="site-main" role="main"> 
+    <article class="article single-page" id="<?php the_ID(); ?>">
+        <header class="block">
+            <div class="content">
+                <?php
+                if ( has_post_thumbnail() ) {
+                        echo get_the_post_thumbnail( null, 'full', [ 'class' => 'background-hero', 'alt'   => get_the_title(), 'loading' => 'lazy', 'data-speed' => '0.25' ] );
+                    }
+
+                    the_title( '<h1 class="page-title">', '</h1>' );
+
+                    if ( get_the_modified_time('d/m/Y') ) {
+                        echo '<p class="latest-modified">' . esc_html__( 'Este archivo fue modificado por última vez el ', 'stories' ) . get_the_modified_time('d/m/Y') . '</p>';
+                    }
+                ?>
+            </div>
+        </header>
+        <section class="block">
+            <div class="is-layout-constrained">
+                <?php the_content(); ?>
+            </div>
+        </section>
+    </article>
+</div>
