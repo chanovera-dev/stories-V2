facb955cf08de107019a6233bf5404e727d9090b
Chano Vera <github@chanovera.com>
2025-11-01 16:37:59 -0600

    fix scapes for customizer

--- footer.php
+++ footer.php
@@ -3,7 +3,16 @@
                 <div class="content">
                     <div class="about">
                         <h3 class="title-section">Sobre <?php bloginfo( 'title' ); ?></h3>
-                        <p class="section"><?php echo get_theme_mod('stories_bio', 'Relatos y Cartas es un espacio dedicado a la creatividad y la expresión a través de las palabras. Aquí encontrarás cuentos, microcuentos, poemas e historias que buscan inspirar, emocionar y conectar con los lectores.'); ?></p>
+                        <p class="site-bio">
+                            <?php
+                            echo esc_html(
+                                get_theme_mod(
+                                    'stories_bio',
+                                    __('Relatos y Cartas es un espacio dedicado a la creatividad y la expresión a través de las palabras. Aquí encontrarás cuentos, microcuentos, poemas e historias que buscan inspirar, emocionar y conectar con los lectores.', 'stories')
+                                )
+                            );
+                            ?>
+                        </p>
                         <?php
                             wp_nav_menu(
                                 array(
