a26a1c3e369f24261b28f2065eeda3eff133652c
Chano Vera <github@chanovera.com>
2025-11-02 06:06:42 -0600

    add the single post format for image and aside, css basic

--- single.php
+++ single.php
@@ -19,7 +19,23 @@ if ( have_posts() ) {
 
     while ( have_posts() ) {
         the_post();
-        get_template_part( 'template-parts/content', 'single' );
+        // Detectar formato de post; si no hay formato usar 'single' como fallback.
+        $post_format = get_post_format();
+        $suffix = 'single'; // por defecto buscamos template-parts/content-single.php
+
+        if ( $post_format ) {
+            // construimos el sufijo que queremos: 'single-{format}'
+            $maybe = 'single-' . $post_format;
+
+            // comprobamos si existe template-parts/content-single-{format}.php
+            if ( locate_template( "template-parts/content-{$maybe}.php" ) ) {
+                $suffix = $maybe;
+            }
+        }
+
+        // Esto cargará template-parts/content-single.php o
+        // template-parts/content-single-{format}.php si existía.
+        get_template_part( 'template-parts/content', $suffix );
     }
 
 }
--- template-parts/content-single-aside.php
+++ template-parts/content-single-aside.php
@@ -1,53 +1,35 @@
 <div id="main" class="site-main" role="main">
     <article class="article single-post" id="<?php the_ID(); ?>">
-        <header class="block">
-            <div class="content">
-                <?php
-                    if ( has_post_thumbnail() ) {
-                        echo get_the_post_thumbnail( null, 'full', [ 'class' => 'background-hero', 'alt'   => get_the_title(), 'loading' => 'lazy', 'data-speed' => '0.25' ] );
-                    }
-
-                    the_title( '<h1 class="page-title">', '</h1>' );
-                    echo '<div class="metadata"><div class="date">' . get_the_date() . '</div>';
-                    if ( get_comments_number() > 0 ) :
-                        echo '<div class="comments">';
-                                if ( get_comments_number() == 1 ) {
-                                    echo get_comments_number(); echo '<span></span>' . esc_html( 'Comentario', 'stories' );
-                                } else {
-                                    echo get_comments_number(); echo '<span></span>' . esc_html( 'Comentarios', 'stories' );
-                                }
-                        echo '</div>';
-                    endif;
-                ?>
-            </div>
-        </header>
-        <section class="block">
-            <div class="content categories">
-                <?php
-                    $categories = get_the_category();
-                    if ( ! empty( $categories ) ) {
-                        foreach ( $categories as $category ) {
-                            // Escapar el nombre y generar link seguro
-                            $cat_name = esc_html( $category->name );
-                            $cat_link = esc_url( get_category_link( $category->term_id ) );
-
-                            echo "<a href='{$cat_link}' class='tag-type small-text'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-bookmark\" viewBox=\"0 0 16 16\"><path d=\"M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z\"/></svg>{$cat_name}</a> ";
-                        }
-                    }
-                ?>
-            </div>
+        <section class="block">    
             <div class="is-layout-constrained">
                 <?php the_content(); ?>
             </div>
-            <div class="content content-tags">
-                <?php
-                    $tags = get_the_tags();
-                    if ( $tags ) {
-                        foreach ( $tags as $tag ) {
-                            echo '<a class="tag-type small-text" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16"><path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0"/><path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1m0 5.586 7 7L13.586 9l-7-7H2z"/></svg>' . esc_html( $tag->name ) . '</a>';
+            <div class="content date categories tags">
+                <div class="date"><?= get_the_date(); ?></div>
+                <div class="categories">
+                    <?php
+                        $categories = get_the_category();
+                        if ( ! empty( $categories ) ) {
+                            foreach ( $categories as $category ) {
+                                // Escapar el nombre y generar link seguro
+                                $cat_name = esc_html( $category->name );
+                                $cat_link = esc_url( get_category_link( $category->term_id ) );
+
+                                echo "<a href='{$cat_link}' class='tag-type small-text'><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-bookmark\" viewBox=\"0 0 16 16\"><path d=\"M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z\"/></svg>{$cat_name}</a> ";
+                            }
                         }
-                    }
-                ?>
+                    ?>
+                </div>
+                <div class="tags">
+                    <?php
+                        $tags = get_the_tags();
+                        if ( $tags ) {
+                            foreach ( $tags as $tag ) {
+                                echo '<a class="tag-type small-text" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tag" viewBox="0 0 16 16"><path d="M6 4.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-1 0a.5.5 0 1 0-1 0 .5.5 0 0 0 1 0"/><path d="M2 1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2a1 1 0 0 1 1-1m0 5.586 7 7L13.586 9l-7-7H2z"/></svg>' . esc_html( $tag->name ) . '</a>';
+                            }
+                        }
+                    ?>
+                </div>
             </div>
             <div class="content content-author">
                 <?php
