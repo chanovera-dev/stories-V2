bdcdcaaa2ba80194f91305199d847e08e77d6fdf
Chano Vera <github@chanovera.com>
2025-11-02 19:09:01 -0600

    add basic video format post

---
+++ template-parts/content-single-video.php
@@ -0,0 +1,28 @@
+<div id="main" class="site-main" role="main">
+    <article class="article single-post" id="<?php the_ID(); ?>">
+        <header class="block">
+            <div class="content">
+                <?php
+                    the_content();
+                ?>
+            </div>
+        </header>
+        <section class="block image-metadata">
+            <?php
+                foreach ( [ 'date-categories-tags', 'author', 'single-post-pagination' ] as $part ) {
+                    get_template_part( 'templates/single/' . $part );
+                }
+            ?>
+        </section>
+        <?php 
+            get_template_part( 'templates/single/related', 'posts' );
+            if ( comments_open() ): 
+        ?>
+        <section class="block">
+            <div class="content content-comments">
+                <?php comments_template(); ?>
+            </div>
+        </section>
+        <?php endif; ?>
+    </article>
+</div>
