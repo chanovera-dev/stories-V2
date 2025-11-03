6f8db50e32db869dce056090ed1d31626278a392
Chano Vera <github@chanovera.com>
2025-11-02 19:28:29 -0600

    change in the tree animation

--- home.php
+++ home.php
@@ -37,6 +37,7 @@ get_header(); ?>
             <div class="tree--wrapper">
                 <img class="tree" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree.webp" alt="tree" width="600" height="984" loading="lazy">
                 <img class="tree-leaves" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves.webp" alt="tree" width="600" height="984" loading="lazy">
+                <img class="tree-leaves-2" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves-2.webp" alt="tree" width="600" height="984" loading="lazy">
             </div>
         </div>
     </header>
