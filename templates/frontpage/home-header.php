10877669ef373638eda48b31ea2888b2ead09283
Chano Vera <github@chanovera.com>
2025-11-03 15:42:28 -0600

    change tree pictures for min pictures

--- templates/frontpage/home-header.php
+++ templates/frontpage/home-header.php
@@ -14,9 +14,9 @@
         </div>
         <div class="stars"></div>
         <div class="tree--wrapper">
-            <img class="tree" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree.webp" alt="tree" width="600" height="984" loading="lazy">
-            <img class="tree-leaves" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves.webp" alt="tree" width="600" height="984" loading="lazy">
-            <img class="tree-leaves-2" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves-2.webp" alt="tree" width="600" height="984" loading="lazy">
+            <img class="tree" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-min.webp" alt="tree" width="600" height="984" loading="lazy">
+            <img class="tree-leaves" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves-min.webp" alt="tree" width="600" height="984" loading="lazy">
+            <img class="tree-leaves-2" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves-2-min.webp" alt="tree" width="600" height="984" loading="lazy">
         </div>
         <div class="container">
             <div class="slideshow">
