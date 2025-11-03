c3f19268eb2c5910bd0c43b01404eed542780887
Chano Vera <github@chanovera.com>
2025-11-03 06:31:54 -0600

    separate home header on apart file

--- home.php
+++ home.php
@@ -18,32 +18,13 @@ get_header(); ?>
 
 <main id="main" class="site-main" role="main">
 
-    <?php if ( ! is_paged() ) : ?>
-    <header class="block">
-        <div class="content">
-            <div class="clouds--wrapper">
-                <div class="clouds">
-                    <div class="c1 one"></div>
-                    <div class="c1 two"></div>
-                    <div class="c1 three"></div>
-                    <div class="c1 four"></div>
-                    <div class="c2 one"></div>
-                    <div class="c2 two"></div>
-                    <div class="c2 three"></div>
-                    <div class="c2 four"></div>
-                </div>
-            </div>
-            <div class="stars"></div>
-            <div class="tree--wrapper">
-                <img class="tree" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree.webp" alt="tree" width="600" height="984" loading="lazy">
-                <img class="tree-leaves" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves.webp" alt="tree" width="600" height="984" loading="lazy">
-                <img class="tree-leaves-2" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves-2.webp" alt="tree" width="600" height="984" loading="lazy">
-            </div>
-        </div>
-    </header>
-    <?php else : ?>
-    <?php wp_breadcrumbs(); ?>
-    <?php endif; ?>
+    <?php 
+        if ( ! is_paged() ) :
+            get_template_part( 'templates/frontpage/home', 'header' );
+        else :
+            wp_breadcrumbs();
+        endif; 
+    ?>
 
     <!-- Archive Posts Section -->
     <section class="block posts--body">
---
+++ templates/frontpage/home-header.php
@@ -0,0 +1,22 @@
+<header class="block">
+    <div class="content">
+        <div class="clouds--wrapper">
+            <div class="clouds">
+                <div class="c1 one"></div>
+                <div class="c1 two"></div>
+                <div class="c1 three"></div>
+                <div class="c1 four"></div>
+                <div class="c2 one"></div>
+                <div class="c2 two"></div>
+                <div class="c2 three"></div>
+                <div class="c2 four"></div>
+            </div>
+        </div>
+        <div class="stars"></div>
+        <div class="tree--wrapper">
+            <img class="tree" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree.webp" alt="tree" width="600" height="984" loading="lazy">
+            <img class="tree-leaves" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves.webp" alt="tree" width="600" height="984" loading="lazy">
+            <img class="tree-leaves-2" src="<?= esc_url( get_template_directory_uri() ); ?>/assets/img/tree-leaves-2.webp" alt="tree" width="600" height="984" loading="lazy">
+        </div>
+    </div>
+</header>
