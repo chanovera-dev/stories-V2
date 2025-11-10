b726d2b9146c1ee11c73bd172a6a0498e1202c69
Chano Vera <github@chanovera.com>
2025-11-09 19:56:48 -0600

    new content-dde

--- template-parts/content-dde.php
+++ template-parts/content-dde.php
@@ -1,10 +1,12 @@
-<article class="post dde">
+<article class="post">
     <div class="post--body">
-        <div class="post--body__header">
+        <div class="post--body__content">
             <a href="<?php the_permalink();?>" class="post--permalink">
                 <?php the_title('<h3 class="post--title">', '</h3>'); ?>
             </a>
-            <?php the_excerpt(); ?>
+            <div class="text">
+                <?php the_excerpt(); ?>
+            </div>
         </div>
     </div>
 </article>
