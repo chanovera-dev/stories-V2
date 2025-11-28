7098c035d7b3c353840e345cd47da212bc1c872d
Chano Vera <github@chanovera.com>
2025-11-28 14:39:59 -0600

    better accesibility

--- templates/archive-property/filter-properties.php
+++ templates/archive-property/filter-properties.php
@@ -3,22 +3,26 @@
     <form class="property-filter-form" id="property-filters">
         <!-- Search -->
         <div class="filter">
-            <input type="text" name="search" placeholder="<?php esc_html_e('Palabras clave', 'stories'); ?>">
+            <input type="text" id="filter-search" name="search" aria-label="<?php esc_attr_e('Palabras clave', 'stories'); ?>" placeholder="<?php esc_html_e('Palabras clave', 'stories'); ?>">
         </div>
 
         <!-- Operation -->
-        <div class="menu-flex">
-            <div class="filter-property"><input type="checkbox" name="operation[]" value="sale"><label><?= stories_get_metadata_icon('sale'); esc_html_e('Venta', 'stories'); ?></label></div>
-            <div class="filter-property"><input type="checkbox" name="operation[]" value="rental"><label><?= stories_get_metadata_icon('rent'); esc_html_e('Renta', 'stories'); ?></label></div>
-        </div>
+        <!-- Operation -->
+        <fieldset class="menu-flex" style="border:0; padding:0; margin:0;">
+            <legend class="screen-reader-text" style="position:absolute; width:1px; height:1px; padding:0; margin:-1px; overflow:hidden; clip:rect(0,0,0,0); border:0;"><?php esc_html_e('Tipo de Operación', 'stories'); ?></legend>
+            <div class="filter-property"><input type="checkbox" id="op-sale" name="operation[]" value="sale"><label for="op-sale"><?= stories_get_metadata_icon('sale'); esc_html_e('Venta', 'stories'); ?></label></div>
+            <div class="filter-property"><input type="checkbox" id="op-rent" name="operation[]" value="rental"><label for="op-rent"><?= stories_get_metadata_icon('rent'); esc_html_e('Renta', 'stories'); ?></label></div>
+        </fieldset>
 
         <!-- Type -->
-        <div class="menu-flex">
-            <div class="filter-property"><input type="checkbox" name="type[]" value="casa"><label><?= stories_get_metadata_icon('house'); esc_html_e('Casa', 'stories'); ?></label></div>
-            <div class="filter-property"><input type="checkbox" name="type[]" value="bedroom"><label><?= stories_get_metadata_icon('bedroom'); esc_html_e('Habitación', 'stories'); ?></label></div>
+        <!-- Type -->
+        <fieldset class="menu-flex" style="border:0; padding:0; margin:0;">
+            <legend class="screen-reader-text" style="position:absolute; width:1px; height:1px; padding:0; margin:-1px; overflow:hidden; clip:rect(0,0,0,0); border:0;"><?php esc_html_e('Tipo de Propiedad', 'stories'); ?></legend>
+            <div class="filter-property"><input type="checkbox" id="type-casa" name="type[]" value="casa"><label for="type-casa"><?= stories_get_metadata_icon('house'); esc_html_e('Casa', 'stories'); ?></label></div>
+            <div class="filter-property"><input type="checkbox" id="type-bedroom" name="type[]" value="bedroom"><label for="type-bedroom"><?= stories_get_metadata_icon('bedroom'); esc_html_e('Habitación', 'stories'); ?></label></div>
             <!-- <div class="filter-property"><input type="checkbox" name="type[]" value="apartment"><label><?= stories_get_metadata_icon('construction'); esc_html_e('Depto', 'stories'); ?></label></div> -->
-            <div class="filter-property"><input type="checkbox" name="type[]" value="terreno"><label><?= stories_get_metadata_icon('garden'); esc_html_e('Terreno', 'stories'); ?></label></div>
-        </div>
+            <div class="filter-property"><input type="checkbox" id="type-terreno" name="type[]" value="terreno"><label for="type-terreno"><?= stories_get_metadata_icon('garden'); esc_html_e('Terreno', 'stories'); ?></label></div>
+        </fieldset>
 
         <ul class="filter-navigation menu">
 
@@ -43,9 +47,9 @@
 
             <!-- Location -->
             <li class="menu-item-has-children filter state">
-                <button class="btn button-for-submenu" type="button" aria-label="Open submenu for ubication">
+                <button class="btn button-for-submenu" type="button" aria-expanded="false" aria-label="<?php esc_attr_e('Abrir submenú para ubicación', 'stories'); ?>">
                     <?php esc_html_e('Ubicación', 'stories'); ?>
-                    <svg width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
+                    <svg aria-hidden="true" width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 
                             .708.708l-6 6a.5.5 0 0 1-.708 
@@ -54,12 +58,14 @@
                 </button>
                 <ul class="sub-menu">
                     <div class="backdrop"></div>
-                    <?php foreach ($locations as $state => $cities): ?>
+                    <?php foreach ($locations as $state => $cities): 
+                        $state_id = 'state-' . sanitize_title($state);
+                    ?>
                         <li class="menu-item-has-children">
                             <div class="btn wrapper-for-title">
-                                <p class="checkbox-filter-properties"><input type="checkbox" name="state[]" value="<?php echo esc_attr($state); ?>"><label><?php echo esc_html($state); ?></label></p>
-                                <button class="button-for-submenu" type="button"  aria-label="Open submenu for ubication of second level">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                <p class="checkbox-filter-properties"><input type="checkbox" id="<?php echo esc_attr($state_id); ?>" name="state[]" value="<?php echo esc_attr($state); ?>"><label for="<?php echo esc_attr($state_id); ?>"><?php echo esc_html($state); ?></label></p>
+                                <button class="button-for-submenu" type="button" aria-expanded="false" aria-label="<?php printf(esc_attr__('Abrir submenú para %s', 'stories'), $state); ?>">
+                                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                                     </svg>
@@ -67,8 +73,10 @@
                             </div>
                             <ul class="sub-menu">
                                 <div class="backdrop"></div>
-                                <?php foreach ($cities as $city): ?>
-                                    <li><p class="checkbox-filter-properties"><input type="checkbox" name="city[]" value="<?php echo esc_attr($city); ?>"><label><?php echo esc_html($city); ?></label></p></li>
+                                <?php foreach ($cities as $city): 
+                                    $city_id = 'city-' . sanitize_title($city);
+                                ?>
+                                    <li><p class="checkbox-filter-properties"><input type="checkbox" id="<?php echo esc_attr($city_id); ?>" name="city[]" value="<?php echo esc_attr($city); ?>"><label for="<?php echo esc_attr($city_id); ?>"><?php echo esc_html($city); ?></label></p></li>
                                 <?php endforeach; ?>
                             </ul>
                         </li>
@@ -78,9 +86,9 @@
 
             <!-- Rooms -->
             <li class="menu-item-has-children filter rooms">
-                <button class="btn button-for-submenu" type="button"  aria-label="Open submenu for room type">
+                <button class="btn button-for-submenu" type="button" aria-expanded="false" aria-label="<?php esc_attr_e('Abrir submenú para habitaciones', 'stories'); ?>">
                     <?php esc_html_e('Habitaciones', 'stories'); ?>
-                    <svg width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
+                    <svg aria-hidden="true" width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M1.646 4.646a.5.5 0 0 1 
                             .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 
@@ -111,9 +119,9 @@
 
             <!-- Price -->
             <li class="menu-item-has-children filter price">
-                <button class="btn button-for-submenu" type="button"  aria-label="Open submenu for price">
+                <button class="btn button-for-submenu" type="button" aria-expanded="false" aria-label="<?php esc_attr_e('Abrir submenú para precio', 'stories'); ?>">
                     <?php esc_html_e('Precio', 'stories'); ?>
-                    <svg width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
+                    <svg aria-hidden="true" width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"></path>
                     </svg>
                 </button>
@@ -122,12 +130,12 @@
                     <div class="backdrop"></div>
                     <li> 
                         <div>
-                            <label><?php esc_html_e('Mínimo', 'stories'); ?></label>
-                            <input type="number" name="price_min" placeholder="<?php esc_html_e('Min $', 'stories'); ?>" min="0" value="<?php echo esc_attr(get_query_var('price_range')['min'] ?? 0); ?>">
+                            <label for="price_min"><?php esc_html_e('Mínimo', 'stories'); ?></label>
+                            <input type="number" id="price_min" name="price_min" placeholder="<?php esc_html_e('Min $', 'stories'); ?>" min="0" value="<?php echo esc_attr(get_query_var('price_range')['min'] ?? 0); ?>">
                         </div>  
                         <div>
-                            <label><?php esc_html_e('Máximo', 'stories'); ?></label>    
-                            <input type="number" name="price_max" placeholder="<?php esc_html_e('Max $', 'stories'); ?>" min="0" value="<?php echo esc_attr(get_query_var('price_range')['max'] ?? 0); ?>">
+                            <label for="price_max"><?php esc_html_e('Máximo', 'stories'); ?></label>    
+                            <input type="number" id="price_max" name="price_max" placeholder="<?php esc_html_e('Max $', 'stories'); ?>" min="0" value="<?php echo esc_attr(get_query_var('price_range')['max'] ?? 0); ?>">
                         </div>
 
                         <div class="price-range">
@@ -141,9 +149,9 @@
 
             <!-- Size (Construction + Land) -->
             <li class="menu-item-has-children filter size">
-                <button class="btn button-for-submenu" type="button" aria-label="Open submenu for price">
+                <button class="btn button-for-submenu" type="button" aria-expanded="false" aria-label="<?php esc_attr_e('Abrir submenú para medidas', 'stories'); ?>">
                     <?php esc_html_e('Medidas (m²)', 'stories'); ?>
-                    <svg width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
+                    <svg aria-hidden="true" width="13" height="13" fill="currentColor" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 
                             .708.708l-6 6a.5.5 0 0 1-.708 
@@ -158,8 +166,14 @@
                     <li>
                         <label><?php esc_html_e('Construcción (m²)', 'stories'); ?></label>
                         <?php $construction_range = get_query_var('construction_range'); ?>
-                        <div><input type="number" name="construction_min" placeholder="<?php esc_html_e('Min m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($construction_range['min'] ?? 0); ?>"></div>
-                        <div><input type="number" name="construction_max" placeholder="<?php esc_html_e('Max m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($construction_range['max'] ?? 0); ?>"></div>
+                        <div>
+                            <label for="construction_min" class="screen-reader-text" style="position:absolute; width:1px; height:1px; padding:0; margin:-1px; overflow:hidden; clip:rect(0,0,0,0); border:0;"><?php esc_html_e('Min Construcción', 'stories'); ?></label>
+                            <input type="number" id="construction_min" name="construction_min" placeholder="<?php esc_html_e('Min m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($construction_range['min'] ?? 0); ?>">
+                        </div>
+                        <div>
+                            <label for="construction_max" class="screen-reader-text" style="position:absolute; width:1px; height:1px; padding:0; margin:-1px; overflow:hidden; clip:rect(0,0,0,0); border:0;"><?php esc_html_e('Max Construcción', 'stories'); ?></label>
+                            <input type="number" id="construction_max" name="construction_max" placeholder="<?php esc_html_e('Max m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($construction_range['max'] ?? 0); ?>">
+                        </div>
                         <div class="construction-range">
                             <label for="construction_range"><?php esc_html_e('Rango construcción:', 'stories'); ?></label>
                             <input type="range" id="construction_range" name="construction_range" min="<?php echo esc_attr($construction_range['min'] ?? 0); ?>" max="<?php echo esc_attr($construction_range['max'] ?? 1000); ?>" step="10" value="<?php echo esc_attr($construction_range['min'] ?? 100); ?>">
@@ -171,8 +185,14 @@
                     <li>
                         <label><?php esc_html_e('Terreno (m²)', 'stories'); ?></label>
                         <?php $land_range = get_query_var('land_range'); ?>
-                        <div><input type="number" name="land_min" placeholder="<?php esc_html_e('Min m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($land_range['min'] ?? 0); ?>"></div>
-                        <div><input type="number" name="land_max" placeholder="<?php esc_html_e('Max m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($land_range['max'] ?? 0); ?>"></div>
+                        <div>
+                            <label for="land_min" class="screen-reader-text" style="position:absolute; width:1px; height:1px; padding:0; margin:-1px; overflow:hidden; clip:rect(0,0,0,0); border:0;"><?php esc_html_e('Min Terreno', 'stories'); ?></label>
+                            <input type="number" id="land_min" name="land_min" placeholder="<?php esc_html_e('Min m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($land_range['min'] ?? 0); ?>">
+                        </div>
+                        <div>
+                            <label for="land_max" class="screen-reader-text" style="position:absolute; width:1px; height:1px; padding:0; margin:-1px; overflow:hidden; clip:rect(0,0,0,0); border:0;"><?php esc_html_e('Max Terreno', 'stories'); ?></label>
+                            <input type="number" id="land_max" name="land_max" placeholder="<?php esc_html_e('Max m²', 'stories'); ?>" min="0" value="<?php echo esc_attr($land_range['max'] ?? 0); ?>">
+                        </div>
                         <div class="land-range">
                             <label for="land_range"><?php esc_html_e('Rango terreno:', 'stories'); ?></label>
                             <input type="range" id="land_range" name="land_range" min="<?php echo esc_attr($land_range['min'] ?? 0); ?>" max="<?php echo esc_attr($land_range['max'] ?? 2000); ?>" step="10" value="<?php echo esc_attr($land_range['min'] ?? 200); ?>">
