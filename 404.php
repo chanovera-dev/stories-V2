4517f24096a232cf067b0fadfbdb18de6e7e2b45
Chano Vera <github@chanovera.com>
2025-12-02 09:29:42 -0600

    changes svg for a function

--- 404.php
+++ 404.php
@@ -15,15 +15,13 @@ get_header(); ?>
     <section class="block not-found__wrapper">
         <div class="content not-found">
             <div class="not-found__icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                </svg>
+                <?= stories_get_metadata_icon('close'); ?>
             </div>
             <h2><?php esc_html_e( 'Página no encontrada.', 'stories' ); ?></h2>
 
             <!-- Back to Homepage Button -->
-            <button class="btn primary" onclick="window.location.href='<?php echo site_url(); ?>'" aria-name="Link to go home">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2"><path d="M5 12.76c0-1.358 0-2.037.274-2.634c.275-.597.79-1.038 1.821-1.922l1-.857C9.96 5.75 10.89 4.95 12 4.95s2.041.799 3.905 2.396l1 .857c1.03.884 1.546 1.325 1.82 1.922c.275.597.275 1.276.275 2.634V17c0 1.886 0 2.828-.586 3.414S16.886 21 15 21H9c-1.886 0-2.828 0-3.414-.586S5 18.886 5 17z"/><path stroke-linecap="round" stroke-linejoin="round" d="M14.5 21v-5a1 1 0 0 0-1-1h-3a1 1 0 0 0-1 1v5"/></g></svg>
+            <button class="btn primary" onclick="window.location.href='<?= site_url(); ?>'" aria-name="Link to go home">
+                <?= stories_get_metadata_icon('home'); ?>
                 <?php esc_html_e( 'Ir al inicio', 'stories' ); ?>
             </button>
         </div>
--- template-parts/single/content-single.php
+++ template-parts/single/content-single.php
@@ -27,7 +27,7 @@
                                 // Escapar el nombre y generar link seguro
                                 $cat_name = esc_html( $category->name );
                                 $cat_link = esc_url( get_category_link( $category->term_id ) );
-                                $cat_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-bookmark" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8"/><path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/><path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/></svg>';
+                                $cat_icon = stories_get_metadata_icon('category');
                                 echo "<a href='{$cat_link}' class='post-tag'>{$cat_icon}<span>{$cat_name}</span></a> ";
                             }
                         }
@@ -35,8 +35,7 @@
                 </div>
                 <?php
                     the_title( '<h1 class="page-title">', '</h1>' );
-                    $date_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar4-week" viewBox="0 0 16 16"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/><path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/></svg>';
-                    echo '<div class="metadata"><div class="date">' . $date_icon . get_the_date() . '</div>';
+                    echo '<div class="metadata"><div class="date">' . stories_get_metadata_icon('date') . get_the_date() . '</div>';
                     if ( get_comments_number() > 0 ) :
                         echo '<div class="comments">';
                                 if ( get_comments_number() == 1 ) {
