a79ba6d75ea6ff997d5e6696512479f0b6406e64
Chano Vera <github@chanovera.com>
2025-12-03 17:42:10 -0600

    changes on UI for post gallery

--- inc/custom-blocks.php
+++ inc/custom-blocks.php
@@ -60,13 +60,9 @@ function custom_render_gallery_block($block_content, $block) {
             <?php endforeach; ?>
         </div>
         <div class="post-gallery-thumbs-container">
-            <?php
-                $prev_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"></path></svg>';
-                $next_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path></svg>';
-            ?>
-            <button class="btn-pagination small-pagination" aria-label="Anterior"><?= $prev_icon; ?></button>
+            <button class="btn-pagination" aria-label="Anterior"><?= stories_get_metadata_icon('backward'); ?></button>
             <div class="post-gallery-thumbs"></div>
-            <button class="btn-pagination small-pagination" aria-label="Siguiente"><?= $next_icon; ?></button>
+            <button class="btn-pagination" aria-label="Siguiente"><?= stories_get_metadata_icon('forward'); ?></button>
         </div>
     </div>
 
